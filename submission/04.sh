41b231d469a23b5db4822f4e35ca28f6c089d909
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-22 12:10:55 +0300

    refactor  sol 4 some more

--- submission/04.sh
+++ submission/04.sh
@@ -1,8 +1,4 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-LOCKTIME=1495584032
-LE_HEX=$(printf '%08x' "$LOCKTIME" | sed 's/\(..\)\(..\)\(..\)\(..\)/\4\3\2\1/')
-
-SCRIPT="04${LE_HEX}b1752102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277ac"
-
-printf '%s' "$SCRIPT"
+#!/bin/bash
+printf '%s' "0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
